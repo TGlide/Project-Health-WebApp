a26efd0aeb1841f051317e340643ea717acce507
TGLIDE <thomglopes@gmail.com>
2019-06-24 17:12:13 -0300

    add pressao

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,4 +1,4 @@
-var ctx = document.getElementById('medChart').getContext('2d');
+var ctx = document.getElementById('medChartBPM').getContext('2d');
 ctx.height = 100;
 var chart = new Chart(ctx, {
     // The type of chart we want to create
@@ -22,6 +22,58 @@ var chart = new Chart(ctx, {
                 ticks: {
                     suggestedMin: 60,
                     suggestedMax: 100
+                }
+            }]
+        },
+        responsive: true,
+        mantainAspectRatio: false
+    }
+});
+
+var ctx = document.getElementById('medChartPre').getContext('2d');
+ctx.height = 100;
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto'],
+        datasets: [{
+            label: 'Pressão Sistólica',
+            yAxisID: 'S',
+            backgroundColor: 'rgba(255, 99, 132, 0.0)',
+            borderColor: '#F44336',
+            data: [120, 115, 130, 110, 112, 114, 95, 130]
+        },
+        {
+            label: 'Pressão Diastólica',
+            yAxisID: 'D',
+            backgroundColor: 'rgba(255, 99, 132, 0.0)',
+            borderColor: '#0288D1',
+            data: [72, 65, 72, 60, 75, 80, 70, 80]
+        }
+        ]
+    },
+
+    // Configuration options go here
+    options: {
+        scales: {
+            yAxes: [{
+                id: 'S',
+                type: 'linear',
+                position: 'left',
+                ticks: {
+                    min: 80,
+                    max: 180
+                }
+            }, {
+                id: 'D',
+                type: 'linear',
+                position: 'right',
+                ticks: {
+                    min: 60,
+                    max: 110
                 }
             }]
         },
