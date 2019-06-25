0ee29495487bfa77565c109767405cbd0ab1d852
TGLIDE <thomglopes@gmail.com>
2019-06-25 15:54:15 -0300

    Coisas aleatorias

--- medications/views.py
+++ medications/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from users.models import Caregiver, Patient
+from helpers import logged
 
 def meds_all(request):
+    if not logged:
+        return redirect('dashboard:login')
+    caregiver = Caregiver.objects.get(id=request.session['caregiver'])
+    
+    
+
     return render(request, 'meds_all.pug')
--- users/models.py
+++ users/models.py
@@ -9,6 +9,8 @@ class Caregiver(models.Model):
     def __str__(self):
         return "Caregiver {}".format(self.name)
     
+    def has_patients(self):
+        return len(self.patient_set.all())
 
 
 class Patient(models.Model):
@@ -16,6 +18,7 @@ class Patient(models.Model):
     name = models.CharField(max_length=42)
     email = models.EmailField(max_length=128)
     password = models.CharField(max_length=256)
+    age = models.IntegerField()
 
     def __str__(self):
         return "Patient {}".format(self.name)
