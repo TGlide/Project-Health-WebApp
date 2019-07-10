0ee94e5f06b8a98bd65f6648a14b896a35b2cb5f
tglide <thomglopes@gmail.com>
2019-07-10 20:29:41 -0300

    Some shit

--- project_health/settings.py
+++ project_health/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os
+import os, django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -147,3 +147,4 @@ STATICFILES_DIRS = (
 )
 
 # Additonal Settings
+django_heroku.settings(locals())
